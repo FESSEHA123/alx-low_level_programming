da970fc05de99e07899ce119517e3ecfd8b3c220
FESSEHA123 <fessehazena@gmail.com>
2022-12-16 12:42:02 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - entry poit
+ * main - Entry Point
+ *
  * Return: Always 0 (sucess)
-*/
+ */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -11,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-}	
+}
