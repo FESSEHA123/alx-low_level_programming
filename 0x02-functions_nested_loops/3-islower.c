d32c0e98a3716a6dd05a5c33fb65392a70ac28c5
FESSEHA123 <fessehazena@gmail.com>
2022-12-30 15:55:41 -0800

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 
 /**
  * _islower - checks for lower case character
- * @c: the character to be checked 
+ * @c: the character to be checked
  * Return: 1 for lowercase charcter or 0 for anything else
  */
 int _islower(int c)
