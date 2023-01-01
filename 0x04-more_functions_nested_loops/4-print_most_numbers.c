e45535bf46233e06065fc05ae1821f107211420f
FESSEHA123 <fessehazena@gmail.com>
2023-01-01 13:02:41 -0800

    task 3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,16 @@
 /**
  * print_most_numbers - print num except 2 and 4
  *
- *Return:
+ *Return:void 
  */
 void print_most_numbers(void)
-{
-	char c;
+{	
+char c;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
-	}
-	_putchar ('\n');
+for (c = '0'; c <= '9'; c++)
+{
+if (!(c == '2' || c == '4'))
+_putchar(c);
+}
+_putchar ('\n');
 }
