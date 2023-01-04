15144f07b689c4465bfe92b0dea943964ba657b9
FESSEHA123 <fessehazena@gmail.com>
2023-01-03 21:03:11 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string  - captionalize all word ofas tring
+ * @str: the sbting o fchaperhlixe
+ * Return: a poinetr too the  chjanged
+ */
+char *cap_string(char *str)
+
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[inedx] <= 'z'))
+		index++;
+		if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+		str[index] -= 32;
+		index++;
+	}
+	return (str)
+}
