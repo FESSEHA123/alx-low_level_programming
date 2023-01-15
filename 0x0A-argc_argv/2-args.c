63c67925928e52d99a41cb93a9170bc59f51f2ca
FESSEHA123 <fessehazena@gmail.com>
2023-01-14 19:25:54 -0800

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints scgkdcbhdc h	bccldcc
+ * @argc: jhjhwgjcgpe
+ * @argv: argument vecto
+ *
+ * Return: always zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
